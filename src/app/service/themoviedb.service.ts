8434fb7fa851c7f46662ebdd60772aac46ccbffd
Mr-Dany <toshibaqosmiox75@hotmail.com>
2020-07-28 00:12:13 -0500

    queriendo fusionar dinamicamente
    filtro y consumir dinamico nombre y paginas

--- src/app/service/themoviedb.service.ts
+++ src/app/service/themoviedb.service.ts
@@ -5,17 +5,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ThemoviedbService {
   constructor(private http: HttpClient) { }
-  getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=ed10bd8c6a2daeb3b35740779b186c66&language=en-US&query=inside%20out&page=1&include_adult=false')
-  }
-}
-/*
-export class ThemoviedbService {
-  constructor(private http: HttpClient) { }
-  getMovies() {
-    let url='https://api.themoviedb.org/3/search/movie?api_key=ed10bd8c6a2daeb3b35740779b186c66&language=en-US&query=inside%20out&page=1&include_adult=false';
+  getMovies(nameMovie:string,page:number) {
+    let url='https://api.themoviedb.org/3/search/movie?api_key=ed10bd8c6a2daeb3b35740779b186c66&language=en-US&query='+nameMovie+'&page='+page+'&include_adult=false';
     return new Promise((resolve,rejects) => {
-      this.http.get<any[]>(url)
+      this.http.get(url)
       .subscribe(data => {
         resolve(data);
         }, (error) => {
@@ -24,5 +17,3 @@ export class ThemoviedbService {
     });
   }
 }
-
-*/
